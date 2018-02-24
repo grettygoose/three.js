87d7d78159dc4b177b1cd63e3c11c9ed7d011805
grettygoose <gheinric@fredhutch.org>
2018-02-23 16:09:00 -0800

    updates

--- script.js
+++ script.js
@@ -1,52 +1,62 @@
- // ------------------------------------------------
- // BASIC SETUP
- // ------------------------------------------------
+// ------------------------------------------------
+// BASIC SETUP
+// ------------------------------------------------
 
-
- // 1. render, scene, camera
- // 2. geometry, material, mesh
+//  STEPS
+// 1. render, scene, camera
+// 2. geometry, material, mesh
 
 // 1.
 
 // Create a renderer with Antialiasing
-var renderer = new THREE.WebGLRenderer({antialias:true}); //smooths jagged edges
+var renderer = new THREE.WebGLRenderer({ antialias: true }); //smooths jagged edges
 // rebder size
-renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
-	
-  var scene = new THREE.Scene();
-  
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var scene = new THREE.Scene();
+
 // //Configure scene color
-scene.background = new THREE.Color( 0x000000 );
-  
-  var camera = new THREE.PerspectiveCamera(
-		75,			// Field of view
-		800 / 640,	// Aspect ratio
-		0.1,		// Near
-		10000		// Far
-	);
-	camera.position.set( 15, 10, 15 );
-  camera.lookAt( scene.position );
-  var geometry = new THREE.BoxGeometry( 5, 5, 5 );
-	var material = new THREE.MeshLambertMaterial( { color: 0xFF0000 } );
-	var mesh = new THREE.Mesh( geometry, material );
-  scene.add( mesh );
-  var light = new THREE.PointLight( 0xFFFF00 );
-	light.position.set( 10, 0, 10 );
-  scene.add( light );
-  
-  
-  
-  var render = function () {
-    requestAnimationFrame( render );
-  
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
-  
-    // Render the scene
-    renderer.render(scene, camera);
-  };
-  render();
+scene.background = new THREE.Color(0x000000);
+
+var camera = new THREE.PerspectiveCamera(
+  75,			// Field of view
+  window.innerWidth / window.innerHeight,	// Aspect ratio
+  0.1,		// Near
+  10000		// Far
+);
+
+// camera's XYZ coordinates
+camera.position.set(15, 10, 15);
+
+//camera at the scene object position
+camera.lookAt(scene.position);
+
+
+//2.
+//geometry + material = mesh
+var geometry = new THREE.TorusBufferGeometry(10, 3, 16, 100 );
+var material = new THREE.MeshLambertMaterial({ color: 0x348442, wireframe: true, emissive: 0x61316b });
+var mesh = new THREE.Mesh(geometry, material);
+//mesh is added to scene
+scene.add(mesh);
+
+
+var light = new THREE.PointLight(0xFFFF00);
+light.position.set(100, 0, 10);
+scene.add(light);
+
+//animation
+var render = function () {
+  requestAnimationFrame(render);
+
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+
+  // Render the scene
+  renderer.render(scene, camera);
+};
+render();
 
 
 
@@ -72,10 +82,10 @@ scene.background = new THREE.Color( 0x000000 );
 //   //The third and fourth parameters specify cut-off points for objects in the camera's view. 
 //   0.1, 	// Near
 //   1000 ); // Far
-  
+
 //   // x, y,z coordinates 
 //   camera.position.set( 15, 10, 4 );
-  
+
 //   //points the camera at the scene object position
 //   camera.lookAt( scene.position );
 
@@ -93,12 +103,12 @@ scene.background = new THREE.Color( 0x000000 );
 
 
 //   var render = function () {
-  
-    
+
+
     // Render the scene
   //   renderer.render(scene, camera);
   // };
-  
+
   // render();
 
 
